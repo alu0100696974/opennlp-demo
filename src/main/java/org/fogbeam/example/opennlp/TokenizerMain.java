6a8a311855cda5b118c2506c437e8cb99be64c3e
alu0100696974 <alu0100696974@ull.edu.es>
2016-05-28 11:36:33 +0100

    Lectura

--- src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
+++ src/main/java/org/fogbeam/example/opennlp/TokenizerMain.java
@@ -18,7 +18,49 @@ public class TokenizerMain
 		
 		// the provided model
 		// InputStream modelIn = new FileInputStream( "models/en-token.bin" );
+        try {
+		archivo = new File("C:\\Users\\Ayoze//archivo.txt");//"archivo.txt" es el archivo que va a leer
+		String linea;
+		//List<String> palabras= new ArrayList(); //Creacion del array
+		Integer l= new Integer(1);
+            Map<String,Integer> listaPalabras = new HashMap();
+            try (FileReader fr = new FileReader (archivo)) {
+                BufferedReader br = new BufferedReader(fr);
+                
 
+                int a=2;
+                while((linea=br.readLine())!=null) {
+                    StringTokenizer st = new StringTokenizer(linea);
+                    while(st.hasMoreTokens()){
+                        String pal= st.nextToken();
+                   //palabras.add(st.nextToken());    // Introducir las palabras en un array            
+                   if(listaPalabras.containsKey(pal)){
+                       listaPalabras.put(pal, a);
+                   }else{
+                       listaPalabras.put(pal, l); 
+                   }
+                    }
+                }
+                      int b=0;
+		Iterator<String> it = listaPalabras.keySet().iterator();
+		while(it.hasNext()){
+		String key = it.next();
+		if(listaPalabras.get(key)<2){
+		b++;
+		System.out.println("Clave: " + key + " -> Valor: " + listaPalabras.get(key));
+			} 
+		} System.out.println("Son "+ b + " palabras sin repetir");
+
+                //System.out.println("La parabra: " + palabras); //Muestra las palabras del array
+               // System.out.println("La parabra: " + listaPalabras);
+
+            }
+
+	}
+	catch(IOException a){
+	System.out.println(a);
+	}
+		
 		
 		// the model we trained
 		InputStream modelIn = new FileInputStream( "models/en-token.model" );
